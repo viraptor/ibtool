d6074d83005e2ff200165ad186ec52cb264b7442
Stanisław Pitucha <git@viraptor.info>
2024-11-06 22:12:02 +1100

    Update testing
    Can't use Apple's ibtool to compile each time, because it's weird and breaks
    open fd-s of other apps. Impressive crap.

--- test.sh
+++ test.sh
@@ -1,11 +1,26 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
+
+test_out=samples/.test_out
+test_dump=samples/.test_dump
+orig_dump=samples/.orig_dump
 
 for xib in samples/*.xib ; do
 	echo "Testing $xib"
-	python ibtool.py --compile samples/output_test.nib "$xib"
-	if diff --side-by-side <(python ibtool.py --dump "${xib/\.xib/.nib}") <(python ibtool.py --dump samples/output_test.nib) ; then
+
+	python ibtool.py --compile "$test_out" "$xib"
+
+	python ibtool.py --dump "${xib/xib/nib}" > "$orig_dump"
+	python ibtool.py --dump "$test_out" > "$test_dump"
+
+	sleep 1
+	diff -u "$orig_dump" "$test_dump"
+	if [[ $? == 0 ]] ; then
+		sleep 1
 		echo "ok"
 	else
+		sleep 1
 		echo "failed"
 	fi
+	rm -f "$test_out" "$test_dump" "$orig_dump"
 done
+
