58588829d4fa2f626bc2c43e339729739e53e87d
Stanisław Pitucha <git@viraptor.info>
2025-07-21 20:30:26 +0930

    box defaults

--- ibtool/parsers/box.py
+++ ibtool/parsers/box.py
@@ -9,11 +9,12 @@ def parse(ctx: ArchiveContext, elem: Element, parent: NibObject) -> None:
     obj["NSSuperview"] = obj.xib_parent()
     parse_children(ctx, elem, obj)
     _xibparser_common_translate_autoresizing(ctx, elem, parent, obj)
+    obj["IBNSBoxIsUsingDocumentContentView"] = True
     obj["NSBorderType"] = {
         "separator": 3,
         None: 3,
     }[elem.attrib.get("boxType")]
-    obj["NSBoxType"] = 2
+    obj["NSBoxType"] = 0
     obj["NSSubviews"] = NibMutableList([])
     obj["NSTitleCell"] = NibObject("NSTextFieldCell", None, {
         "NSBackgroundColor": makeSystemColor("textBackgroundColor"),
@@ -33,7 +34,7 @@ def parse(ctx: ArchiveContext, elem: Element, parent: NibObject) -> None:
     if "verticalHuggingPriority" in obj.extraContext or "horizontalHuggingPriority" in obj.extraContext:
         v, h = obj.extraContext.get("verticalHuggingPriority", 250), obj.extraContext.get("horizontalHuggingPriority", 250)
         obj["NSHuggingPriority"] = f"{{{h}, {v}}}"
-    obj["NSOffsets"] = NibString.intern("{5, 5}")
+    obj["NSOffsets"] = NibString.intern("{0, 0}")
     if not obj.extraContext.get("parsed_autoresizing"):
         obj.flagsOr("NSvFlags", vFlags.DEFAULT_VFLAGS_AUTOLAYOUT if ctx.useAutolayout else vFlags.DEFAULT_VFLAGS)
 
