1b582c5a8f3d3bff3c53ec3e91cc07b9160e5a4c
Stanisław Pitucha <git@viraptor.info>
2025-07-26 21:15:29 +0930

    Make installable

--- ibtool/__main__.py
+++ ibtool/__main__.py
@@ -1,13 +1,32 @@
-from . import ibtool
 from . import ibdump
+from . import ibtool
 from . import compare
 import sys
+import argparse
 
-if sys.argv[1] == "--compile":
-    ibtool.main()
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    parser.add_argument("--compile", metavar="output")
+    parser.add_argument("--compare", metavar="input2")
+    parser.add_argument("--dump", action="store_true")
+    parser.add_argument("-e", "--encoding", action="store_true", help="Show encoding")
+    parser.add_argument("-t", "--tree", action="store_true", help="Show tree")
+    parser.add_argument("-s", "--sort", action="store_true", help="Sort keys")
+    args = parser.parse_args()
 
-elif sys.argv[1] == "--compare":
-    compare.main(sys.argv[2], sys.argv[3])
+    if args.compile:
+        ibtool.ib_compile(args.input, args.compile)
 
-elif sys.argv[1] == "--dump":
-    ibdump.ibdump(sys.argv[2])
+    elif args.compare:
+        compare.main(args.compare, args.input)
+
+    elif args.dump:
+        ibdump.ibdump(args.input, args.encoding, args.tree, args.sort)
+
+    else:
+        parser.print_help()
+        sys.exit()
+
+if __name__ == "__main__":
+    run()
--- ibtool/ibtool.py
+++ ibtool/ibtool.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import getopt
 import sys
 import os
 import xml.etree.ElementTree as ET
@@ -10,70 +9,16 @@ from . import ibdump
 from . import xibparser
 
 
-class IBCommands:
-    Compile = 0
-    Dump = 1
-
-
-def main():
-    ops, args = getopt.getopt(sys.argv[1:], "ets", ["compile=", "write=", "dump"])
-    if len(args) == 0:
-        print("Error: No input file given.")
-        sys.exit(1)
-    elif len(args) > 1:
-        print("Error: ibtool currently only supports one input file.")
-        sys.exit(1)
-
-    command = IBCommands.Dump
-    inpath = args[0]
-
-    _write = None
-    _compile = None
-    shortflags = []
-
-    for option, value in ops:
-        if option == "--compile":
-            command = IBCommands.Compile
-            _compile = value
-        elif option == "--write":
-            _write = value
-        elif option == "--dump":
-            command = IBCommands.Dump
-        elif option == "-e":
-            shortflags.append("e")
-        elif option == "-t":
-            shortflags.append("t")
-        elif option == "-s":
-            shortflags.append("s")
-
-    if command is None:
-        print("Error: No command given.")
-        sys.exit(1)
-
-    if command == IBCommands.Compile:
-        ib_compile(inpath, _write or _compile)
-    elif command == IBCommands.Dump:
-        ib_dump(inpath, shortflags)
-
-
 def ib_compile(inpath, outpath):
-    def die_if(condition, message):
-        if condition:
-            print(message)
-            exit(1)
-
-    die_if(not outpath, "ib_compile: No input path given")
-
     suffix = None
     if inpath.endswith(".xib"):
         suffix = "xib"
     elif inpath.endswith(".storyboard"):
         suffix = "storyboard"
 
-    die_if(
-        suffix is None,
-        "ib_compile: Only .xib and .storyboard files are currently supported.",
-    )
+    if suffix is None:
+        sys.exit("ib_compile: Only .xib and .storyboard files are currently supported.")
+
     if suffix == "xib":
         ib_compile_xib(inpath, outpath)
     elif suffix == "storyboard":
@@ -98,14 +43,3 @@ def ib_compile_xib(inpath, outpath):
 def ib_compile_storyboard(inpath, outpath):
     tree = ET.parse(inpath)
     xibparser.CompileStoryboard(tree, outpath)
-
-
-def ib_dump(inpath, shortflags):
-    showencoding = "e" in shortflags
-    showTree = "t" in shortflags
-    sortKeys = "s" in shortflags
-    ibdump.ibdump(inpath, showencoding, showTree, sortKeys)
-
-
-if __name__ == "__main__":
-    main()
