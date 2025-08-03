1082f0137ccfce28d0a9a01bcf6f2261c76b9246
Stanisław Pitucha <git@viraptor.info>
2025-08-03 10:56:21 +0930

    defaults

--- ibtool/parsers/userDefaultsController.py
+++ ibtool/parsers/userDefaultsController.py
@@ -4,11 +4,12 @@ from .helpers import make_xib_object, handle_props, PropSchema, MAP_YES_NO
 from ..parsers_base import parse_children
 
 def parse(ctx: ArchiveContext, elem: Element, parent: NibObject) -> XibObject:
-    obj = make_xib_object(ctx, "NSUserDefaultsController", elem, parent)
+    obj = make_xib_object(ctx, "NSUserDefaultsController", elem, parent, False)
 
     parse_children(ctx, elem, obj)
     
     handle_props(ctx, elem, obj, [
+        PropSchema("NSSharedInstance", const=True)
     ])
 
     return obj
